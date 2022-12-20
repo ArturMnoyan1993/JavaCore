365f4cd064df5a02d125caca3a744c50d0764d14
ArturMnoyan1993 <arturmnoyan2022@gmail.com>
2022-12-21 00:52:01 +0400

    Homework1

---
+++ src/homework1/IfForExample.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 5, b = 15;
+        if (b > a) {
+            System.out.println(b);
+        }
+    }
+}
---
+++ src/homework1/IfForExample2.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class IfForExample2 {
+    public static void main(String[] args) {
+        int a = 5, b = 15; // b-ի արժեքը նշված է, որպիսի հնարավոր
+        // լինի պարզել խնդիրը ճիշտ լուծված է, թե ոչ:
+        for (a = 5; a <= 15; a++) {
+            System.out.println(+a);
+        }
+    }
+}
