a8b8a5de9c774ce43fcca78daaef9b8ded813ef3
ArturMnoyan1993 <arturmnoyan2022@gmail.com>
2022-12-24 17:09:08 +0400

    Homework1

--- src/homework1/FigurPainter2.java
+++ src/homework1/FigurPainter2.java
@@ -2,20 +2,14 @@ package homework1;
 
 public class FigurPainter2 {
     public static void main(String[] args) {
-        for (int k = 4; k > 0 ; k--) {
-            System.out.print("xy");
-            for (int a = 1; a <=k; a++) {
-                System.out.print("y");
-            }
-            System.out.println();
-        }
         for (int i = 0; i < 5; i++) {
-            System.out.print("y");
-            for (int j = 0; j <= 4; j++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-
     }
 }
