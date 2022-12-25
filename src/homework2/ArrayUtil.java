1f286b308528cb5f98fcf609d9bb86c8d76b047e
ArturMnoyan1993 <arturmnoyan2022@gmail.com>
2022-12-25 20:28:28 +0400

    Homework1

---
+++ src/homework2/ArrayUtil.java
@@ -0,0 +1,30 @@
+package homework2;
+
+public class ArrayUtil {
+
+    public static void main(String[] args) {
+        int[] array = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("max: " + max);
+
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min: " + min);
+    }
+}
