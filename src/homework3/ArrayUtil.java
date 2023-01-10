80d03a5541fa59410ff540113317d5e223d011d9
ArturMnoyan1993 <arturmnoyan2022@gmail.com>
2023-01-10 22:49:46 +0400

    կատարված առաջադրանքներ

---
+++ src/homework3/ArrayUtil.java
@@ -0,0 +1,38 @@
+package homework3;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+
+//        for (int x : array) {
+//            if (x % 2 == 0) {
+//            }
+//            System.out.print(x + " ");
+//        }
+
+//        System.out.println();
+
+//        for (int x : array) {
+//            if (x % 2 != 0) {
+//                System.out.print(x + " ");
+//            }
+
+//        int evenCount = 0;
+//        for (int x : array) {
+//            if (x % 2 == 0) {
+//                evenCount++;
+//            }
+//        }
+//        System.out.println(+evenCount);
+
+//        int oddCount = 0;
+//        for (int x : array) {
+//            if (x % 2 != 0) {
+//                oddCount++;
+//            }
+//        }
+//        System.out.println(+oddCount);
+
+//        System.out.println(array[array.length / 2]);
+    }
+}
